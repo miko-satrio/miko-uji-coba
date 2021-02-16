8362ee6f020e6979607f87f2fb9044595153b7ca
miko-satrio <79110334+miko-satrio@users.noreply.github.com>
2021-02-17 03:01:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+export default {
+  ucapan: [
+    "hey kamu, tau gak bahwa perasaan aku tuh melebihi apapun yang kamu tau",
+    "Harta, Tahta, Afifah >.<",
+    "Hey! I Love You:)",
+  ],
+  panggilan: ["Afifah"],
+  colors: [
+    "#FF6900",
+    "#FCB900",
+    "#7BDCB5",
+    "#00D084",
+    "#8ED1FC",
+    "#0693E3",
+    "#ABB8C3",
+    "#EB144C",
+    "#F78DA7",
+    "#000000",
+  ],
+};
